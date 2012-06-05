6576e0473b05e5c502de8e779d6d785149da730e
Drew Bowman <bowmande@gmail.com>
2012-06-05 14:16:37 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyTracker::Application.config.secret_token = 'cbaf6c8cc8e290fd9e76a9652beabf2f2bedd1a64c5f296c5729d7344d586b6ef70a1d9644d70bcf070b28b44f93c20caefb63129a70e413730b040f40b65d41'
